8e10628684b528e0c80e8aa004c2fa73d719102c
Rodrigo Basichetti Martins <rodrigo.basichetti@up.edu.br>
2021-07-12 21:53:30 -0300

    Inicio Alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles} from '../src/lib/AluraCommons'
 
 const GlobalStyle = createGlobalStyle`
+
   body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  ${AlurakutStyles}
+
 `
 
 const theme = {
